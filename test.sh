07443eab5b179f360aba7c2d791910b46a345141
Andriy Buryanovaty <andrew.buryanovaty@viaduct.pro>
2023-03-28 17:27:18 +0300

    update collection/ enviroment. add able to change creds from env. Doent close terminal after execution

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # set variables
-path_to_collection_file="/Users/andrew/Documents/APIAutotests/APIPostmanTest/collection.json"
-path_to_environment_file="/Users/andrew/Documents/APIAutotests/APIPostmanTest/environment.json"
+path_to_collection_file="F:\OEPostmanAPITest\APIPostmanTest\collection.json"
+path_to_environment_file="F:\OEPostmanAPITest\APIPostmanTest\environment.json"
 your_project_code="OET"
 your_api_token="662083c9ecc566fd0cb6798b3838e3a5d7c30507178190a9b5f7ac36d7f65562"
-your_run_id="6"
+your_run_id="28"
 your_base_path="https://api.qase.io/v1"
 your_run_name="My Postman Collection"
 your_run_description="A test run of my Postman Collection"
@@ -24,3 +24,5 @@ newman run "$path_to_collection_file" \
     --reporter-qase-runName "$your_run_name" \
     --reporter-qase-runDescription "$your_run_description" \
     --reporter-qase-rootSuiteTitle "$your_root_suite_title"
+
+$SHELL
